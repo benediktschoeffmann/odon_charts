d2f1db65cec070e06ff572ecacda38c5b0e38d58
Luka A. <l.anic3108@gmail.com>
2025-07-03 20:07:38 +0200

    commit

--- config/databaseconfig.ts
+++ config/databaseconfig.ts
@@ -1,11 +1,12 @@
-import mariadb from 'mariadb';
+import mariadb from "mariadb";
+import "dotenv/config";
 
 const dbpool = mariadb.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    connectionLimit: 5
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  //password: process.env.DB_PASSWORD,
+  database: process.env.DB_HOST,
+  connectionLimit: 5,
 });
 
 export default dbpool;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import express, { Application } from "express";
 
-import dbpool from '../config/databaseconfig';
+import dbpool from "../config/databaseconfig";
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 9000;
 
 const app: Application = express();
 
@@ -10,6 +10,22 @@ app.get("/ping", async (_req, res) => {
   res.send({
     message: "pong",
   });
+});
+
+app.get("/api/songs", (_req, res, next) => {
+  console.log("trying to connect")
+  dbpool.getConnection().then((conn) =>
+    conn
+      .query("SELECT * FROM songs")
+      .then((result) => {
+        res.json(result);
+    })
+      .catch((err) => {
+        res.status(500).json({
+          message: "Error Code 500"
+        })
+      })
+  );
 });
 
 app.listen(PORT, () => {
