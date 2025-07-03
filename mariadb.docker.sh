486809c15338a02864116d6b138357cd3f9b9b92
Ben Schöffmann <benedikt.schoeffmann@gmail.com>
2025-07-03 20:41:09 +0200

    Chore: update docker starrt scripts

--- mariadb.docker.sh
+++ mariadb.docker.sh
@@ -4,8 +4,6 @@ docker run --detach --name mariadb\
     -p ${MARIADB_PORT}:3306\
     --add-host=host.docker.internal:host-gateway\
     -v ./dbdata:/var/lib/mysql\
-    --env MARIADB_USER=${MARIADB_USER}\
-    --env MARIADB_PASSWORD=${MARIADB_PASSWORD}\
-    --env MARIADB_ROOT_PASSWORD=${MARIADB_ROOT_PASSWORD}\
+    --env MARIADB_ALLOW_EMPTY_ROOT_PASSWORD=1\
     --env MARIADB_DATABASE=${MARIADB_DATABASE}\
     mariadb:10.11
