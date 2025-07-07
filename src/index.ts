bc164ecd184d580683d4bfc36577a1fc8af2d3b1
Luka A. <l.anic3108@gmail.com>
2025-07-07 09:42:22 +0200

    added path to get songs by artistName

--- src/index.ts
+++ src/index.ts
@@ -31,8 +31,8 @@ app.get("/api/songs", (_req, res, next) => {
   );
 });
 
-app.get("/api/song", (req, res) => {
-  const songTitle = req.query.title as string;
+app.get("/api/song/title/:title", (req, res) => {
+  const songTitle = req.params.title as string;
 
   if (!songTitle) {
      res.status(400).json({
@@ -79,7 +79,7 @@ app.get("/api/songs/year/:releaseYear", (req, res) => {
 
   dbpool.getConnection().then((conn) =>
     conn
-      .query("SELECT * FROM `songs` WHERE `releaseYear` BETWEEN ? AND ?;",
+      .query("SELECT title, releaseYear FROM `songs` WHERE `releaseYear` BETWEEN ? AND ?;",
         [`${releaseYear}-01-01`, `${releaseYear}-12-31`,])
       .then((result) => {
         if (result.length > 0) {
@@ -103,7 +103,7 @@ app.get("/api/songs/year/:releaseYear", (req, res) => {
 
 app.get("/api/songs/betweenYear/:firstYear/:lastYear", (req, res) => {
   const firstYear = parseInt(req.params.firstYear, 10);
-  const lastYear = parseInt(req.params.lastYear, 10)
+  const lastYear = parseInt(req.params.lastYear, 10);
 
   if (!firstYear || !lastYear || !Number.isInteger(firstYear) || !Number.isInteger(lastYear)) {
     res.status(400).json({
@@ -115,7 +115,7 @@ app.get("/api/songs/betweenYear/:firstYear/:lastYear", (req, res) => {
 
   dbpool.getConnection().then((conn) =>
     conn
-      .query("SELECT * FROM `songs` WHERE `releaseYear` BETWEEN ? AND ?;", [
+      .query("SELECT title, releaseYear FROM `songs` WHERE `releaseYear` BETWEEN ? AND ?;", [
         `${firstYear}-01-01`,
         `${lastYear}-12-31`,
       ])
@@ -137,6 +137,47 @@ app.get("/api/songs/betweenYear/:firstYear/:lastYear", (req, res) => {
         conn.end();
       })
   );
+});
+
+app.get("/api/songs/artist/:artistName", (req, res) => {
+  const artistName = req.params.artistName as string;
+
+  if (!artistName) {
+    res.status(400).json({
+      message: "Valid parameter required",
+    });
+    return;
+  }
+
+  dbpool.getConnection().then((conn) => {
+      conn
+        .query(
+          "SELECT title, releaseYear FROM `songs` " +
+            "INNER JOIN songs_artists ON songs.ID = songs_artists.songID " +
+            "INNER JOIN artists ON songs_artists.artistID = artists.ID " +
+            "WHERE LOWER(artists.name) = LOWER(?);",
+          [artistName]
+        )
+        .then((result) => {
+          if (result.length > 0) {
+            console.log("there are some results");
+            res.json(result);
+          } else {
+            res.status(404).json({
+              message: "Song not found",
+            });
+          }
+        })
+        .catch((err) => {
+          console.error("Database error:", err);
+          res.status(500).json({
+            message: "Internal server error",
+          });
+        })
+        .finally(() => {
+          conn.end();
+        });
+  });
 });
 
 app.listen(PORT, () => {
