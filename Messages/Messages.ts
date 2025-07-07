fb894cba8ac908d6be17c9bb084af66d7f160ad7
Luka A. <l.anic3108@gmail.com>
2025-07-07 10:04:20 +0200

    Updated Error Response

--- Messages/Messages.ts
+++ Messages/Messages.ts
@@ -1,13 +1,19 @@
-const ErrorMessages = {
+import { Response } from "express";
+
+const ErrorMessages: Record<number, string> = {
   400: "Invalid request",
   404: "Resource not found",
   500: "Server error",
 };
 
-const sendErrorResponse = (res, statusCode) => {
+const sendErrorResponse = (res: Response, statusCode: keyof typeof ErrorMessages, error?: any) => {
+  error && console.log(error);
   const errorMessage = ErrorMessages[statusCode] || "An Error occured";
 
   res.status(statusCode).json({
     message: errorMessage,
   });
 };
+
+
+export default sendErrorResponse;
