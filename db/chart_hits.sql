09bd785b7fbefaaec953426c29326c5744e3a9aa
Luka A. <l.anic3108@gmail.com>
2025-07-07 11:53:08 +0200

    Added test data in database

--- db/chart_hits.sql
+++ db/chart_hits.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 03, 2025 at 09:18 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Erstellungszeit: 07. Jul 2025 um 11:51
+-- Server-Version: 10.4.32-MariaDB
+-- PHP-Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `chart_hits`
+-- Datenbank: `chart_hits`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `artists`
+-- Tabellenstruktur für Tabelle `artists`
 --
 
 CREATE TABLE `artists` (
@@ -33,10 +33,22 @@ CREATE TABLE `artists` (
   `nationalityID` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Daten für Tabelle `artists`
+--
+
+INSERT INTO `artists` (`ID`, `name`, `nationalityID`) VALUES
+(3, 'Freddie Mercury', 3),
+(4, 'Falco', 2),
+(9, 'Hannes Wirth', 2),
+(10, 'Walther Soyka', 2),
+(11, 'Ernst Molden', 2),
+(12, 'Willi Resetarits', 2);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `charts`
+-- Tabellenstruktur für Tabelle `charts`
 --
 
 CREATE TABLE `charts` (
@@ -49,29 +61,58 @@ CREATE TABLE `charts` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `genres`
+-- Tabellenstruktur für Tabelle `genres`
 --
 
 CREATE TABLE `genres` (
   `ID` int(10) UNSIGNED NOT NULL,
-  `genre` varchar(100) NOT NULL
+  `description` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `genres`
+--
+
+INSERT INTO `genres` (`ID`, `description`) VALUES
+(1, 'Blues'),
+(2, 'Country'),
+(3, 'Easy listening'),
+(4, 'Electronic'),
+(5, 'Folk'),
+(6, 'Hip hop'),
+(7, 'R&B & soul'),
+(8, 'Rock'),
+(9, 'Metal'),
+(10, 'Punk'),
+(11, 'Jazz'),
+(12, 'Pop');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `nationalities`
+-- Tabellenstruktur für Tabelle `nationalities`
 --
 
 CREATE TABLE `nationalities` (
   `ID` int(10) UNSIGNED NOT NULL,
-  `nationality` varchar(50) NOT NULL
+  `description` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `nationalities`
+--
+
+INSERT INTO `nationalities` (`ID`, `description`) VALUES
+(1, 'US'),
+(2, 'AT'),
+(3, 'GB'),
+(5, 'DE'),
+(6, 'JP');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `songs`
+-- Tabellenstruktur für Tabelle `songs`
 --
 
 CREATE TABLE `songs` (
@@ -81,28 +122,44 @@ CREATE TABLE `songs` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `songs`
+-- Daten für Tabelle `songs`
 --
 
 INSERT INTO `songs` (`ID`, `title`, `releaseYear`) VALUES
-(3, 'We Will Rock You', '0000-00-00');
+(3, 'We Will Rock you', '1970-01-22'),
+(4, 'Der Kommissar', '1982-10-27'),
+(5, 'Rock Me Amadeus', '1985-04-25'),
+(6, 'Awarakadawara', '2017-04-19');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `songs_artists`
+-- Tabellenstruktur für Tabelle `songs_artists`
 --
 
 CREATE TABLE `songs_artists` (
-  `keyArtists` int(10) UNSIGNED NOT NULL,
+  `keySongsArtists` int(10) UNSIGNED NOT NULL,
   `songID` int(10) UNSIGNED NOT NULL,
   `artistID` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Daten für Tabelle `songs_artists`
+--
+
+INSERT INTO `songs_artists` (`keySongsArtists`, `songID`, `artistID`) VALUES
+(1, 3, 3),
+(2, 4, 4),
+(3, 5, 4),
+(4, 6, 9),
+(5, 6, 10),
+(6, 6, 11),
+(7, 6, 12);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `songs_genres`
+-- Tabellenstruktur für Tabelle `songs_genres`
 --
 
 CREATE TABLE `songs_genres` (
@@ -112,114 +169,128 @@ CREATE TABLE `songs_genres` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Indexes for dumped tables
+-- Daten für Tabelle `songs_genres`
+--
+
+INSERT INTO `songs_genres` (`keyGenres`, `songID`, `genreID`) VALUES
+(1, 5, 8),
+(2, 5, 12),
+(3, 4, 12),
+(4, 4, 4),
+(5, 6, 2),
+(6, 6, 12);
+
+--
+-- Indizes der exportierten Tabellen
 --
 
 --
--- Indexes for table `artists`
+-- Indizes für die Tabelle `artists`
 --
 ALTER TABLE `artists`
   ADD PRIMARY KEY (`ID`),
   ADD KEY `nationalityID` (`nationalityID`);
 
 --
--- Indexes for table `charts`
+-- Indizes für die Tabelle `charts`
 --
 ALTER TABLE `charts`
   ADD PRIMARY KEY (`ID`),
   ADD KEY `songID` (`songID`);
 
 --
--- Indexes for table `genres`
+-- Indizes für die Tabelle `genres`
 --
 ALTER TABLE `genres`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indexes for table `nationalities`
+-- Indizes für die Tabelle `nationalities`
 --
 ALTER TABLE `nationalities`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indexes for table `songs`
+-- Indizes für die Tabelle `songs`
 --
 ALTER TABLE `songs`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indexes for table `songs_artists`
+-- Indizes für die Tabelle `songs_artists`
 --
 ALTER TABLE `songs_artists`
-  ADD PRIMARY KEY (`songID`,`artistID`),
+  ADD PRIMARY KEY (`keySongsArtists`),
+  ADD KEY `songs_artists_ibfk_2` (`songID`),
   ADD KEY `songs_artists_ibfk_1` (`artistID`);
 
 --
--- Indexes for table `songs_genres`
+-- Indizes für die Tabelle `songs_genres`
 --
 ALTER TABLE `songs_genres`
-  ADD PRIMARY KEY (`songID`,`genreID`),
-  ADD KEY `genreID` (`genreID`);
+  ADD PRIMARY KEY (`keyGenres`),
+  ADD KEY `genreID` (`genreID`),
+  ADD KEY `songID` (`songID`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT für exportierte Tabellen
 --
 
 --
--- AUTO_INCREMENT for table `artists`
+-- AUTO_INCREMENT für Tabelle `artists`
 --
 ALTER TABLE `artists`
-  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT for table `charts`
+-- AUTO_INCREMENT für Tabelle `charts`
 --
 ALTER TABLE `charts`
   MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `genres`
+-- AUTO_INCREMENT für Tabelle `genres`
 --
 ALTER TABLE `genres`
-  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT for table `nationalities`
+-- AUTO_INCREMENT für Tabelle `nationalities`
 --
 ALTER TABLE `nationalities`
-  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `songs`
+-- AUTO_INCREMENT für Tabelle `songs`
 --
 ALTER TABLE `songs`
-  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- Constraints for dumped tables
+-- Constraints der exportierten Tabellen
 --
 
 --
--- Constraints for table `artists`
+-- Constraints der Tabelle `artists`
 --
 ALTER TABLE `artists`
   ADD CONSTRAINT `artists_ibfk_1` FOREIGN KEY (`nationalityID`) REFERENCES `nationalities` (`ID`);
 
 --
--- Constraints for table `charts`
+-- Constraints der Tabelle `charts`
 --
 ALTER TABLE `charts`
   ADD CONSTRAINT `charts_ibfk_1` FOREIGN KEY (`songID`) REFERENCES `songs` (`ID`);
 
 --
--- Constraints for table `songs_artists`
+-- Constraints der Tabelle `songs_artists`
 --
 ALTER TABLE `songs_artists`
   ADD CONSTRAINT `songs_artists_ibfk_1` FOREIGN KEY (`artistID`) REFERENCES `artists` (`ID`) ON DELETE CASCADE,
   ADD CONSTRAINT `songs_artists_ibfk_2` FOREIGN KEY (`songID`) REFERENCES `songs` (`ID`) ON DELETE CASCADE;
 
 --
--- Constraints for table `songs_genres`
+-- Constraints der Tabelle `songs_genres`
 --
 ALTER TABLE `songs_genres`
   ADD CONSTRAINT `songs_genres_ibfk_1` FOREIGN KEY (`genreID`) REFERENCES `genres` (`ID`) ON DELETE CASCADE,
