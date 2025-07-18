f51491be4b8152687da77a50d3a69eab7dc64484
Luka A. <l.anic3108@gmail.com>
2025-07-18 10:59:45 +0200

    Deleted unnecessarily imports in index

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
 import express, { Application, Request, Response } from "express";
 
-import dbpool from "../config/database";
-import sendErrorResponse from "./Responses/ErrorResponse";
-import {
-  getSongsBetweenYearController,
-} from "./controllers/songsController";
-
-
 const PORT = process.env.PORT || 9000;
 
 const app: Application = express();
