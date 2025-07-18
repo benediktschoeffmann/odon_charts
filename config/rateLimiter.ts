c98acb49158ee9b221ff4db99c5b38035d64e74d
Luka A. <l.anic3108@gmail.com>
2025-07-18 10:44:15 +0200

    added rateLimiter

--- config/rateLimiter.ts
+++ config/rateLimiter.ts
@@ -1,2 +1,10 @@
 import rateLimiter from 'express-rate-limit';
 
+const limiter = rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  statusCode: 429,
+  max: 100,
+  message: 'Too many requests, please try again later.'
+});
+
+export default limiter
