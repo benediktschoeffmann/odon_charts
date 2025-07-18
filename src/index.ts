a61e69bc745c8f0089e698bf55a17d0a2fa81f93
Luka A. <l.anic3108@gmail.com>
2025-07-18 10:46:56 +0200

    Added limiter to api

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,18 @@ import express, { Application, Request, Response } from "express";
 import dbpool from "../config/database";
 import sendErrorResponse from "./Responses/ErrorResponse";
 import {
-  getAllSongsController,
   getSongsBetweenYearController,
-  getSongsFromArtistController,
-  getSongsFromGenreController,
-  getSongsFromNationalityController,
-  getSongsFromTitleController,
-  getSongsFromYearController,
 } from "./controllers/songsController";
+
 
 const PORT = process.env.PORT || 9000;
 
 const app: Application = express();
 const songsRouter = require("./routes/songsRoutes");
+import limiter from "../config/rateLimiter";
 
 app.use(songsRouter);
+app.use("/api/", limiter);
 
 //////////////////////SONGS///////////////////////////////////////
 
