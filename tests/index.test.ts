eaf722a67a8f5f9141f452f6492bfad52e98994c
Luka A. <l.anic3108@gmail.com>
2025-07-22 22:53:55 +0200

    Added some test cases for charts

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -128,14 +128,27 @@ describe("Testing index file", () => {
       generalHappyChartCaseTest(
         "GET /api/charts/title/:title should return chart with title",
         "/api/charts/title/Jupiter",
-        {
-          song: expect.objectContaining({ title: "Jupiter" }),
-        },
-        {
-          song: expect.objectContaining({ title: "Ordinary" }),
-        }
+        { song: expect.objectContaining({ title: "Jupiter" }) },
+        { song: expect.objectContaining({ title: "Ordinary" }) }
       );
-      
+      generalHappyChartCaseTest(
+        "GET /api/charts/releaseYear/:releaseYear should return all charts where song released in this year",
+        "/api/charts/releaseYear/2025",
+        { song: expect.objectContaining({ releaseYear: 2025 }) },
+        { song: expect.objectContaining({ releaseYear: 2024 }) }
+      );
+      generalHappyChartCaseTest(
+        "GET /api/charts/year/:year should return charts from year",
+        "/api/charts/year/2025",
+        { year: 2025 },
+        { year: 2024 }
+      );
+      generalHappyChartCaseTest(
+        "GET /api/charts/year/:year/week/:week should return charts from specific year and week",
+        "/api/charts/year/2025/week/28",
+        { week: 28, year: 2025 },
+        { week: 29, year: 2024 }
+      );
     })
 
   });
